19f5be8bfe9bba06b8e746cae4592e7f11c24afc
Sayeed Md. Shaiban <sayeed.k06@gmail.com>
2021-04-12 21:35:44 +0600

    added profile structure

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,8 +2,21 @@ import React from "react";
 import Paper from '@material-ui/core/Paper';
 export default function Profile (){
     return (
-        <div className='profile'>
-            <Paper elevation={3}>
+        <div className='container profile'>
+
+            <div className='row'>
+                <div className='col-3'>
+                <Paper elevation={3}>
+                
+                <h6>Groups</h6>
+                <p>Group 1</p>
+                <p>Group 1</p>
+                <p>Group 1</p>
+                 
+                </Paper>
+                </div>
+                <div className='col-9'>
+                <Paper elevation={3}>
                 <h6>
                     Name: Sayeed Md. Shaiban
                 </h6>
@@ -12,7 +25,11 @@ export default function Profile (){
                 </h6>
                 
                  
-            </Paper>
+                </Paper>
+                </div>
+
+            </div>
+            
         </div>
     );
 }
